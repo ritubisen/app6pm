fddfe79b7bf824e26f653f65ede92e7b8f5c0f41
RITU BISEN <ritub1050@gmail.com>
2025-07-24 19:31:35 +0530

    today update

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
-import Carousel from 'react-bootstrap/Carousel';
-import myimag1 from"./images/r1.jpg"
-import myimag2 from"./images/hb1.jpg"
-import myimag3 from"./images/hb2.jpg"
-
-import Card from 'react-bootstrap/Card';
-import myimag4 from"./images/hb3.jpg"
-
-import Home from "./Home";
 const App=()=>{
+
+  
+  const getval=(e)=>{
+  
+   let name=e.target.name;
+   let myvel=e.target.value;
+   console.log({[name]: value})
+  }
+
     return(
         <>
 
-         <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
- <Carousel>
-      <Carousel.Item>
-      
-      <img src={myimag1} width ="100%" hight=""/> 
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={myimag2} width ="100%" hight=""/> 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-  <img src={myimag3} width ="100%" hight=""/>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+<h1>welcome to my page</h1>
+enter name : <input type="text" />
 
 
- <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          <img src={myimag4} width="300" height="300" />
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-
- <Home/>
-     
         </>
         
     )
